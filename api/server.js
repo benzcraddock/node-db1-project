9d7427b00ced43f9959c5267e32005c9c264cec9
Benjamin Craddock <benjamincraddock@Benjamins-MacBook-Pro.local>
2022-02-14 16:59:21 -0500

    initialize and setup server / router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 
+// router
+const accountsRouter = require('./accounts/accounts-router')
+
 const server = express();
 
 server.use(express.json());
+
+// USE ACCOUNTS
+server.use('/api/accounts', accountsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'api up!'
+  })
+})
 
 module.exports = server;
